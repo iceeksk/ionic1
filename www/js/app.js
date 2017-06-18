fac5e6a857ad3a737db902bbe9dfdb22cc48b4d0
Iceeksk <iceeksk@gmail.com>
2017-06-18 23:20:43 +0300

    update(*): begin create ToDo

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,59 @@
-// Ionic Starter App
+angular.module('ToDo', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.controller("ToDoCtrl", function ($scope, $ionicModal) {
+  $scope.tasks = [
+      {
+        "title": "Learn Ionic",
+        "description": "читать доки, делать таски",
+        "done": "false"
+      },
+      {
+        "title": "Learn Ionic 2",
+        "description": "читать доки, делать таски",
+        "done": "false"
+      },
+      {
+        "title": "Learn Angular 1.x",
+        "description": "читать доки, делать таски",
+        "done": "false"
+      },
+      {
+        "title": "Learn AngularJS",
+        "description": "читать доки, делать таски",
+        "done": "false"
+      },
+      {
+        "title": "Learn Cordova",
+        "description": "читать доки, делать таски",
+        "done": "false"
+      }
+    ];
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  $ionicModal.fromTemplateUrl('templates/task.html', function (modal) {
+    $scope.taskModal = modal;
+  },{
+    scope: $scope,
+    animation: 'slide-in-up'
   });
-})
+
+  $scope.openAddTask = function () {
+    $scope.taskModal.show();
+  }
+
+  $scope.closeAddTask = function () {
+    $scope.taskModal.hide();
+  }
+
+});
